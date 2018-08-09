7f295a84666394f1b653b3619ce105c7143990c5
Raghu Betina <raghu@firstdraft.com>
2018-08-09 00:58:47 +0000

    Install Kaminari

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,7 @@ class PhotosController < ApplicationController
   end
 
   def index
-    @photos = Photo.all
+    @photos = Photo.page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@photos.where.not(:address_tag_latitude => nil)) do |photo, marker|
       marker.lat photo.address_tag_latitude
       marker.lng photo.address_tag_longitude
